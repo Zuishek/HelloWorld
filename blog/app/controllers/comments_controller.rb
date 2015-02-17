61781525f47da917a5c499cac0add5fbaf54f739
Ahmed Abdel-Hamid <a_walid@live.com>
2015-02-17 02:23:49 +0200

    ana brof

---
+++ blog/app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+	def index
+	end
+	def show
+		@comment = Comment.find(params[:id])
+	end
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.create(comment_params)
+		redirect_to article_path(@article)
+	end
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@article)
+	end
+
+	private 
+	def comment_params
+	  params.require(:comment).permit(:commenter, :body)
+	end
+end
